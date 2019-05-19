05ac1be1188ce7df93b1e09bbf47f844758b3417
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-19 17:56:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#welcome' 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' =>'sessions#create'
+
+  
 end
