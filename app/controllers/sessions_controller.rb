0dc2605374223a75a9f8336f17de7ebb47b67852
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-19 17:32:29 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(name: params[:name])
+    @user = User.find_by(name: params[:name])
     session[:user_id] = @user.id
   end
 end
