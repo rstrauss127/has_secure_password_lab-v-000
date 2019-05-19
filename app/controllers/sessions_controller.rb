56efff1bd12d2edb61f7b3109dec3a30c8dc644e
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-19 18:14:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+
+  def new
+  end 
   def create
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    if params[:user][:password] == @user.password
-        session[:user_id] = @user.id
+    session[:user_id] = @user.id
     end
   end
 end
