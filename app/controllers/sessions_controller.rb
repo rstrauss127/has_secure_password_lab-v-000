17720564e13c48c67f3912e9f7ed29131fca85a4
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-19 18:14:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    end
+
   end
 
 end
