08326a24e22a982ffa4c4f0a1d0f017eecf57687
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-19 18:05:20 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(name: params[:name])
-    session[:user_id] = @user.id
+    if params[:password] == params[:password_confirmation]
+      @user = User.find_or_create_by(name: params[:name])
+      session[:user_id] = @user.id
+
+    end
   end
 end
